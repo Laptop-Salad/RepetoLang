3203dc24393fb53c6a3bbcccbf5b0cb179d2672e
Laptop-Salad <awallis1609@gmail.com>
2025-01-25 23:27:42 +0000

    refactor: use try-with-resources

--- repeto/src/main/java/com/lang/repeto/App.java
+++ repeto/src/main/java/com/lang/repeto/App.java
@@ -6,31 +6,24 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        File file;
+        File file = new File(System.getProperty("user.dir") + "/examples/MyAge.repeto");
         StringBuilder lines = new StringBuilder();
-        Scanner sc;
         Lexer lexer;
         Parser parser;
 
-        try {
-            file = new File(System.getProperty("user.dir") + "/examples/MyAge.repeto");
-
-            sc = new Scanner(file);
-
+        try (Scanner sc = new Scanner(file)) {
             while (sc.hasNextLine()) {
                 lines.append(sc.nextLine());
             }
-
-            sc.close();
-
-            lexer = new Lexer(lines.toString());
-            lexer.lex();
-
-            parser = new Parser(lexer.getTokens());
-            parser.parse();
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
             System.exit(0);
         }
+
+        lexer = new Lexer(lines.toString());
+        lexer.lex();
+
+        parser = new Parser(lexer.getTokens());
+        parser.parse();
     }
 }
