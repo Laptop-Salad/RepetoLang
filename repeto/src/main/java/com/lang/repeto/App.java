31f68070c3afdd6eadd2ccb6fc24068b6cdc0680
Laptop-Salad <awallis1609@gmail.com>
2025-01-25 23:08:20 +0000

    refactor: remove helloworld docblock

--- repeto/src/main/java/com/lang/repeto/App.java
+++ repeto/src/main/java/com/lang/repeto/App.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         File file;
